261696170a9e61b8dce17e9e6a1b8773a78b4cda
SotsuHub <124271898+SotsuHub@users.noreply.github.com>
2023-05-09 00:04:47 +0900

    Delete subScript.js

--- subScript.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-// 😊消去ボタン処理
-function pushClearButton() {
-    localStorage.clear();
-    // テーブルの値を消去する
-    inputAllElementsArray.forEach((element) => (element.value = ""));
-    console.log("全て消去しました");
-}
